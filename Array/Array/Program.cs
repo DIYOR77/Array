90b513461e1e451facc89adbd69b4cbc85b84d7a
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-06 10:43:47 +0500

    Arrayni  to'ldirish: 1 o'lshamli.

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Massiv uzunligini kiriting: ");
+            string son=Console.ReadLine();  
+            bool isnumber=int.TryParse(son, out int number);
+            if(isnumber)
+            {
+                int[]arr = new int[number];
+                Fillarray(arr, number);
+                Console.WriteLine();
+            }
+           
+            else
+            {
+                Console.WriteLine(" Faqat butun son kirita olasiz !!! ");
+            }
+            
+            Main(args);
+        }
+        public static int Fillarray(int[] arr, int number)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < number; i++)
+            {
+                arr[i] = random.Next(11, 99);
+                Console.Write(" " + arr[i]);
+            }
+            return number;
         }
     }
 }
